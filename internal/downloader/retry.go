090beba1fe385731fd61dc7d1c6cde38b75e27e6
udaymungalpara <udayhn2005@gmail.com>
2025-07-25 17:55:11 +0530

    update retry

--- internal/downloader/retry.go
+++ internal/downloader/retry.go
@@ -6,23 +6,24 @@ import (
 	"time"
 )
 
-func Retry(Url string, t time.Duration, attemp int) (*http.Response, error) {
-	i := 0
-	var err error
-	for i < attemp {
-		resp, err := http.Get(Url)
+func Retry(url string, delay time.Duration, attempts int) (*http.Response, error) {
+	var lasterr error
+
+	for i := 0; i < attempts; i++ {
+		resp, err := http.Get(url)
 		if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
 			return resp, nil
 		}
 		if resp != nil {
 			resp.Body.Close()
-			fmt.Printf("Retry %d for %s (Status: %d)\n", i+1, Url, resp.StatusCode)
+			lasterr = fmt.Errorf("retry %d for %s (Status: %d)", i+1, url, resp.StatusCode)
 
+		} else {
+			lasterr = err
 		}
 
-		time.Sleep(t)
-		i++
+		time.Sleep(delay)
 	}
-	return nil, fmt.Errorf("all retries failed %v", err)
+	return nil, fmt.Errorf("all retries failed %v", lasterr)
 
 }
